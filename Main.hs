2d9b9a7bf50acaed34a34fd64f2d4c93352dda44
Ian Henry <ianthehenry@gmail.com>
2014-08-05 00:34:03 -0400

    added the ability to add comments

--- Main.hs
+++ Main.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE RecordWildCards #-}
-
 module Main where
 
 import BasePrelude hiding (app)
@@ -7,7 +5,6 @@ import Web.Scotty
 import Network.Wai (Application)
 import qualified Network.Wai.Handler.Warp as Warp
 import Data.Text (Text)
-import qualified Data.Text.Lazy as LT
 import Data.Time.Clock (UTCTime, getCurrentTime)
 import Control.Concurrent.MVar
 import Data.Map (Map)
@@ -30,6 +27,7 @@ type Forum = Map ID Thread
 data Message = Message { messageID :: ID
                        , messageCreator :: User
                        , messageText :: Text
+                       , messageThreadID :: ID
                        , messageTimeStamp :: UTCTime
                        }
 
@@ -43,39 +41,64 @@ instance Aeson.ToJSON Message where
 instance Aeson.ToJSON Thread where
   toJSON Thread {..} = Aeson.object [ "id" .= threadID
                                     , "title" .= threadTitle
-                                    , "messages" .= threadMessages
                                     , "creator" .= threadCreator
                                     , "timeStamp" .= threadTimeStamp
                                     ]
 
+createThread :: MVar Forum -> IO Int -> Text -> Text -> IO Thread
+createThread db nextID title creator = do
+  timeStamp <- liftIO getCurrentTime
+  threadID <- liftIO nextID
+  let thread = Thread { threadID = threadID
+                      , threadTimeStamp = timeStamp
+                      , threadCreator = creator
+                      , threadMessages = []
+                      , threadTitle = title
+                      }
+  modifyMVar db (return . (, thread) . Map.insert threadID thread)
+
+createMessage :: MVar Forum -> IO Int -> Text -> Text -> ID -> IO Message
+createMessage db nextID text creator threadID = do
+  timeStamp <- liftIO getCurrentTime
+  messageID <- liftIO nextID
+  let message = Message { messageID = messageID
+                        , messageTimeStamp = timeStamp
+                        , messageCreator = creator
+                        , messageText = text
+                        , messageThreadID = threadID
+                        }
+  modifyMVar db (return . (, message) . Map.update (Just . addMessage message) threadID)
+  where
+    addMessage message thread@(Thread {threadMessages}) =
+      thread{threadMessages = message:threadMessages}
+
 app :: MVar Forum -> IO Int -> IO Application
 app database nextID = scottyApp $ do
-  get "/threads/:id" $ do
-    threadID <- param "id"
-    entity "Thread not found" =<< Map.lookup threadID <$> db
+  get "/threads/:id" $
+    param "id" >>= withThread json
   get "/threads" $
     json =<< Map.elems <$> db
   post "/threads" $
     flip rescue (\msg -> status status400 >> text msg) $ do
-      title <- param "title"
-      creator <- param "username"
-      timeStamp <- liftIO getCurrentTime
-      threadID <- liftIO nextID
-      let thread = Thread { threadID = threadID
-                          , threadTimeStamp = timeStamp
-                          , threadCreator = creator
-                          , threadMessages = []
-                          , threadTitle = title
-                          }
-      liftIO $ modifyMVar_ database (return . Map.insert threadID thread)
-      json thread
+      [title, creator] <- sequence $ param <$> ["title", "username"] 
+      json =<< liftIO (createThread database nextID title creator)
+  get "/threads/:id/messages" $
+    param "id" >>= withThread (json . threadMessages)
+  post "/threads/:id/messages" $ do
+    threadID <- param "id"
+    flip rescue (\msg -> status status400 >> text msg) $ do
+      [title, creator] <- sequence $ param <$> ["title", "username"] 
+      d <- db
+      if Map.member threadID d then
+        json =<< liftIO (createMessage database nextID title creator threadID)
+      else thread404
 
   where
     db = liftIO (readMVar database)
-    entity msg = maybe (status status404 >> text msg) json
-
---postComment :: Thread -> Thread
-
+    withThread f threadID =
+      maybe thread404 f =<< Map.lookup threadID <$> db
+    thread404 = status status404 >> text "Thread not found"
+      
 twice :: a -> (a, a)
 twice a = (a, a)
 
