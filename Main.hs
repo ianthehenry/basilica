ae8f2bc1701c4c98875a1947431cd017cf8cede2
Ian Henry <ianthehenry@gmail.com>
2014-08-18 22:55:18 -0400

    moved Access-Control-Allow-Origin header into the conf file

--- Main.hs
+++ Main.hs
@@ -2,28 +2,28 @@ module Main where
 
 import           BasePrelude hiding (app)
 import           Control.Monad.Trans (liftIO)
+import           Data.ByteString (ByteString)
 import qualified Data.Configurator as Conf
 import           Database
 import           Network.HTTP.Types
 import           Network.Wai (Application)
+import qualified Network.Wai as Wai
 import qualified Network.Wai.Handler.Warp as Warp
 import           Network.Wai.Handler.WebSockets (websocketsOr)
 import           Network.WebSockets.Connection (defaultConnectionOptions)
 import qualified Sockets
 import           Web.Scotty
 
-accessControl :: ActionM () -> ActionM ()
-accessControl = (>> setHeader "Access-Control-Allow-Origin" "*")
-
-app :: Database -> IO Application
-app db = scottyApp $ do
-  get "/posts/:id" $ accessControl $
+app :: ByteString -> Database -> IO Application
+app origin db = scottyApp $ do
+  middleware (addHeaders [("Access-Control-Allow-Origin", origin)])
+  get "/posts/:id" $
     withPost json =<< param "id"
-  get "/posts" $ accessControl $
+  get "/posts" $
     liftIO (allPosts db) >>= json
-  post "/posts" $ accessControl $
+  post "/posts" $
     with400 $ postPost (return Nothing)
-  post "/posts/:id" $ accessControl $
+  post "/posts/:id" $
     with400 $ postPost (Just <$> param "id")
   where
     withPost f idPost = do
@@ -36,12 +36,19 @@ app db = scottyApp $ do
       maybe post404 json maybePost
     makePost idParent by content = createPost db by content idParent
 
+addHeaders :: ResponseHeaders -> Wai.Middleware
+addHeaders newHeaders app req respond = app req $ \response -> do
+  let (st, currentHeaders, streamHandle) = Wai.responseToStream response
+  streamHandle $ \streamBody ->
+    respond $ Wai.responseStream st (currentHeaders ++ newHeaders) streamBody
+
 main :: IO ()
 main = do
   conf <- Conf.load [Conf.Required "conf"]
   port <- Conf.require conf "port"
+  origin <- Conf.require conf "client-origin"
   db@(_, newPosts) <- newDatabase =<< Conf.require conf "dbpath"
   server <- Sockets.newServer newPosts
-  api <- app db
+  api <- app origin db
   putStrLn $ "Running on port " ++ show port
   Warp.run port (websocketsOr defaultConnectionOptions server api)
