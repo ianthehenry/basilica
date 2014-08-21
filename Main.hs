f8ba2a7b55e2c421aee549fbabbf254a214e766f
Ian Henry <ianthehenry@gmail.com>
2014-08-20 22:25:17 -0400

    include a one-shot login link in the code email

--- Main.hs
+++ Main.hs
@@ -1,11 +1,12 @@
 module Main where
 
-import           BasePrelude hiding (app)
+import           BasePrelude hiding (app, intercalate)
 import           Control.Concurrent.Chan
 import           Control.Monad.Trans (liftIO)
 import           Data.ByteString (ByteString)
 import qualified Data.Configurator as Conf
-import           Data.Text.Lazy (Text)
+import           Data.Text as Strict
+import           Data.Text.Lazy as Lazy
 import           Database
 import           Mailer
 import           Network.HTTP.Types
@@ -17,13 +18,10 @@ import           Network.WebSockets.Connection (defaultConnectionOptions)
 import qualified Sockets
 import           Web.Scotty
 
-maybeParam :: Parsable a => Text -> ActionM (Maybe a)
+maybeParam :: Parsable a => Lazy.Text -> ActionM (Maybe a)
 maybeParam name = (Just <$> param name) `rescue` (return . const Nothing)
 
-emailForCode :: EmailAddress -> CodeRecord -> Email
-emailForCode to CodeRecord{..} = easyEmail to "Your Code" codeValue
-
-basilica :: Maybe ByteString -> Database -> Chan Email -> IO Application
+basilica :: Maybe ByteString -> Database -> Chan (EmailAddress, CodeRecord) -> IO Application
 basilica origin db emailChan = scottyApp $ do
   case origin of
     Nothing -> return ()
@@ -42,7 +40,7 @@ basilica origin db emailChan = scottyApp $ do
       emailAddress <- param "email"
       liftIO $ do
         code <- createCode db emailAddress
-        maybe (return ()) (writeChan emailChan . emailForCode emailAddress) code
+        maybe (return ()) (writeChan emailChan . (emailAddress, )) code
       status status200
   post "/tokens" $
     with400 $ do
@@ -66,16 +64,33 @@ addHeaders newHeaders app req respond = app req $ \response -> do
   streamHandle $ \streamBody ->
     respond $ Wai.responseStream st (currentHeaders ++ newHeaders) streamBody
 
+sendCodeMail :: Mailer -> Strict.Text -> (EmailAddress, CodeRecord) -> IO ()
+sendCodeMail mailer clientUrl (to, CodeRecord{codeValue}) =
+  sendMail mailer (easyEmail to "Basilica Code" messageBody)
+  where messageBody = Strict.intercalate "\n"
+                      [ "Here's your code:"
+                      , ""
+                      , codeValue
+                      , ""
+                      , "And a handy login link:"
+                      , ""
+                      , clientUrl <> "/login?code=" <> codeValue
+                      , ""
+                      , "Love,"
+                      , "  Basilica"
+                      ]
+
 main :: IO ()
 main = do
   conf <- Conf.load [Conf.Required "conf"]
   port <- Conf.require conf "port"
   origin <- Conf.lookup conf "client-origin"
   mailer <- newMailer <$> Conf.require conf "mandrill-key"
+  clientUrl <- Conf.require conf "client-url"
   db <- newDatabase =<< Conf.require conf "dbpath"
   emailChan <- newChan
   server <- Sockets.newServer (dbPostChan db)
   api <- basilica origin db emailChan
-  forkIO $ getChanContents emailChan >>= mapM_ (sendMail mailer)
+  forkIO $ getChanContents emailChan >>= mapM_ (sendCodeMail mailer clientUrl)
   putStrLn $ "Running on port " ++ show port
   Warp.run port (websocketsOr defaultConnectionOptions server api)
